f0022f500de71d32aded443cf133423542c8ad1f
lewa_j <lewa.j20@gmail.com>
2025-05-01 12:00:55 +0300

    Fix copypaste typo in Evaluators

--- src/gl_evaluators.cpp
+++ src/gl_evaluators.cpp
@@ -337,7 +337,7 @@ void APIENTRY glEvalCoord1f(GLfloat u)
 		gl_emit_vertex(gs, glm::make_vec4(p), color, tex_coord, normal);
 	}
 
-	if (gs->eval.enabled_maps & 1 << (map2_index(GL_MAP2_COLOR_4) + 9) && gs->lighting.color_material && gs->lighting.enabled)
+	if (gs->eval.enabled_maps & (1 << map1_index(GL_MAP1_COLOR_4)) && gs->lighting.color_material && gs->lighting.enabled)
 		gs->set_material_color(gs->lighting.color_material_face, gs->lighting.color_material_param, save_color, true);
 }
 
